2dbe15e7dcbef40f249ab16cb43aeaf30061f2ad
Johnson Thomas <johnson9747@gmail.com>
2018-08-03 17:42:45 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { DogPageComponent } from './dog-page/dog-page.component';
+import { CatPageComponent } from './cat-page/cat-page.component';
+import { VideoPageComponent } from './video-page/video-page.component';
+const appRoutes: Routes = [
+  { path: 'dog', component: DogPageComponent },
+  { path: 'cat',component: CatPageComponent },
+  { path: 'video',component: VideoPageComponent },
+  { path: '',
+    redirectTo: '/dog',
+    pathMatch: 'full'
+  },
+  { path: '**', component: DogPageComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DogPageComponent,
+    CatPageComponent,
+    VideoPageComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
